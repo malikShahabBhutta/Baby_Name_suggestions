6ec7f4e4694fec56c6c88a450210ffc6c1f10b2f
malikShahabBhutta <73873661+malikShahabBhutta@users.noreply.github.com>
2023-03-25 01:49:52 +0500

    error handling api

--- JS.js
+++ JS.js
@@ -14,10 +14,22 @@ const gender="neutral";
 const apiURL="https://api.api-ninjas.com/v1/babynames?gender="+gender;
 
 async function GetName(){
+    try {
+        bName.innerText="Updating...";
+    btnElement.disabled = true;
+    btnElement.innerText="Loding...";
     const response=await fetch(apiURL, option)
     const data=await response.json();
+    btnElement.disabled = false;
+    btnElement.innerText="Tell Me Name ";
     bName.innerText=data;
    // console.log(data)
+    } catch (error) {
+        bName.innerText="an error occurred while try later ";
+        btnElement.disabled = false;
+    btnElement.innerText="Tell Me Name ";
+    }
+    
 }
 
 btnElement.addEventListener("click",GetName)
